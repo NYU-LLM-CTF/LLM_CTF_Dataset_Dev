0a58ab6ee9bd5fa4906a751d176da110b085191f
NickNameInvalid <zssmh98@outlook.com>
2024-07-24 10:17:05 -0400

    update 4 challenges, 2 to go

--- 2015/CSAW-Quals/web/k_achieve/arpahack/db.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `users` (
-    `id` INTEGER PRIMARY KEY,
-    `name` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    `counter` INTEGER NOT NULL
-);
-
-CREATE UNIQUE INDEX `users_name_idx` ON `users`(`name`);
